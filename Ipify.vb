50163900cf20076755dc5e148c5a245d1e92c086
SpReeD <wackermann@nexn.systems>
2019-12-14 15:00:32 +0100

    Summary update

--- Ipify.vb
+++ Ipify.vb
@@ -5,29 +5,61 @@ Public Class Ipify
     Private Const ipifyAdressSecure As String = "https://api.ipify.org"
     Private Const ipifyAdressv6 As String = "http://api6.ipify.org"
     Private Const ipifyAdressv6Secure As String = "https://api6.ipify.org"
+
+    ''' <summary>
+    ''' Get public IPv4 Address as System.Net.IPAddress
+    ''' </summary>
+    ''' <param name="useHttps">Using SSL path of ipify.org (HTTPS)</param>
+    ''' <returns>System.Net.IPAddress</returns>
     Public Shared Function GetPublicIPAddress(ByVal Optional useHttps As Boolean = True) As System.Net.IPAddress
         Using wC As WebClient = New WebClient
             GetPublicIPAddress = IPAddress.Parse(wC.DownloadString(If(useHttps, ipifyAdressSecure, ipifyAdress)))
         End Using
     End Function
+
+    ''' <summary>
+    ''' Get public IPv4 Address as String
+    ''' </summary>
+    ''' <param name="useHttps">Using SSL path of ipify.org (HTTPS)</param>
+    ''' <returns>ex. 127.0.0.1</returns>
     Public Shared Function GetPublicAddress(ByVal Optional useHttps As Boolean = True) As String
         Using wC As WebClient = New WebClient
             GetPublicAddress = wC.DownloadString(If(useHttps, ipifyAdressSecure, ipifyAdress))
         End Using
     End Function
 
+    ''' <summary>
+    ''' Get public IPv6 Address as System.Net.IPAddress
+    ''' </summary>
+    ''' <param name="useHttps">Using SSL path of ipify.org (HTTPS)</param>
+    ''' <returns>System.Net.IPAddress</returns>
     Public Shared Function GetPublicIPv6Address(ByVal Optional useHttps As Boolean = True) As System.Net.IPAddress
         Using wC As WebClient = New WebClient
             GetPublicIPv6Address = IPAddress.Parse(wC.DownloadString(If(useHttps, ipifyAdressv6Secure, ipifyAdressv6)))
         End Using
     End Function
+
+    ''' <summary>
+    ''' Get public IPv6 Address as String
+    ''' </summary>
+    ''' <param name="useHttps">Using SSL path of ipify.org (HTTPS)</param>
+    ''' <returns>ex. fe80::1</returns>
     Public Shared Function GetPublicv6Address(ByVal Optional useHttps As Boolean = True) As String
         Using wC As WebClient = New WebClient
             GetPublicv6Address = wC.DownloadString(If(useHttps, ipifyAdressv6Secure, ipifyAdressv6))
         End Using
     End Function
 
+    ''' <summary>
+    ''' Main Class of Geolocation Informmation <br/>
+    ''' Instance this with APIKey as String
+    ''' </summary>
     Public Class GeoIPLocation
+        ''' <summary>
+        ''' Main Class of Geolocation Informmation <br/>
+        ''' Instance this with APIKey as String
+        ''' </summary>
+        ''' <returns></returns>
         Public Property APIKey As String = Nothing
         Private Const ipifyAddress As String = "https://geo.ipify.org/api/v1?apiKey={0}&{1}={2}"
 
@@ -37,6 +69,13 @@ Public Class Ipify
             domain
         End Enum
 
+        ''' <summary>
+        ''' Main Function <br/>
+        ''' Be sure to set your API-Key first!
+        ''' </summary>
+        ''' <param name="QueryValue">What you want to query, usually an IP address</param>
+        ''' <param name="[QueryType]">Typically an ipaddress, but also able to query a domain or email</param>
+        ''' <returns></returns>
         Public Function GetInformation(ByVal QueryValue As String, ByVal Optional [QueryType] As QueryType = QueryType.ipAddress) As IpifyGeoInformation
             Dim acc As IpifyGeoInformation = New IpifyGeoInformation
             Using wC As WebClient = New WebClient
@@ -46,6 +85,9 @@ Public Class Ipify
             Return acc
         End Function
 
+        ''' <summary>
+        ''' Object of JSON deserialization
+        ''' </summary>
         Public Class IpifyGeoInformation
             Public Property Location As IpifyLocation
             Public Property Domains As String()
