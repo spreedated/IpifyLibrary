41319757758910dc0df3d185b2df1911878e253e
SpReeDateD <webmaster@born2skate.de>
2023-03-09 20:12:42 +0100

    refac tests

--- UnitTests/IpifyTests.cs
+++ UnitTests/IpifyTests.cs
@@ -2,29 +2,29 @@ using neXn.IpifyWrapper;
 using neXn.IpifyWrapper.Models;
 using NUnit.Framework;
 using System.Text.RegularExpressions;
+using static UnitTests.Constants;
 
 namespace UnitTests
 {
     public class IpifyTests
     {
-        private const string validateIPv4RegEx = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
-        private const string validateIPv6RegEx = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))";
         [SetUp]
         public void SetUp()
         {
 
         }
 
-        [Test(Description = "Need Internet Connection")]
+        [Test]
+        [Ignore("Need Internet Connection")]
         public void StaticIPv4_6()
         {
             //IPv4
-            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicAddress(), validateIPv4RegEx));
-            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicIPAddress().ToString(), validateIPv4RegEx));
+            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicAddress(), VALIDATEIPV4REGEX));
+            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicIPAddress().ToString(), VALIDATEIPV4REGEX));
 
             //IPv6
-            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicv6Address(), validateIPv6RegEx));
-            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicIPv6Address().ToString(), validateIPv6RegEx));
+            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicv6Address(), VALIDATEIPV6REGEX));
+            Assert.IsTrue(Regex.IsMatch(Ipify.GetPublicIPv6Address().ToString(), VALIDATEIPV6REGEX));
         }
 
         [Test(Description = "Need Internet Connection")]
